27e905bf48f9ef23c1a6525ec79aa7bf9b899705
Denis Dinkov <denisdinkov7@gmail.com>
2023-06-17 00:56:19 +0300

    Update TheResources.vue

--- src/components/learning-recources/TheResources.vue
+++ src/components/learning-recources/TheResources.vue
@@ -1,9 +1,13 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')"
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
       >Stored Resources</base-button
     >
-    <base-button @click="setSelectedTab('add-resource')"
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
       >Add Resources</base-button
     >
   </base-card>
@@ -41,6 +45,14 @@ export default {
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === "stored-resources" ? null : "flat";
+    },
+    addResButtonMode() {
+      return this.selectedTab === "add-resource" ? null : "flat";
     },
   },
   provide() {
